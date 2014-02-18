4016d41ac9be4c31e88c873fc7b258705d01a9d3
4pcbr <me@4pcbr.com>
2014-02-18 23:53:22 +0100

    Added tests assets folder Refactoring of the argv parameters block Implemented index file name argument processing and serving Covered index file serving with test issue
    refs #1

--- lib/staticme.rb
+++ lib/staticme.rb
@@ -3,16 +3,78 @@ require 'rack'
 require 'thin'
 
 module Staticme
+
   extend self
 
-  def run!(argv)
-    pwd = argv['-f'] || argv['--path'] || Dir.pwd
-    port = (argv['-p'] || argv['--port'] || 8080).to_i
-    host = argv['-h'] || argv['--host'] || '0.0.0.0'
-    staticme = Rack::Builder.new do
-      run Rack::Directory.new(pwd)
+  attr_accessor :server
+
+  ARGS  = {
+    :path   => {
+      :short      => 'f',
+      :default    => Proc.new { Dir.pwd }
+    },
+    :port   => {
+      :short    => 'p',
+      :default    => 8080,
+      :sanitizer  => Proc.new { |v| v.to_i }
+    },
+    :host   => {
+      :short      => 'h',
+      :default    => '0.0.0.0'
+    },
+    :index  => {
+      :short      => 'i',
+      :default    => 'index.html'
+    }
+  }
+
+  def run!(argv, &blk)
+
+    params = Hash.new
+
+    ARGS.each_pair do |param_name, param_attrs|
+      param_shorten_name = param_attrs[:shorten]
+      default = param_attrs[:default]
+      sanitizer = param_attrs[:sanitizer]
+      param_value = argv["--#{param_name}"] ||
+                    ( param_shorten_name.nil? ? nil : argv["-#{param_shorten_name}"] ) ||
+                    ( default.is_a?(Proc) ? default.call : default )
+      ( param_value = sanitizer.call( param_value ) ) if sanitizer.is_a? Proc
+
+      if !param_value.nil?
+        params[param_name] = param_value
+      end
+
     end
 
-    Rack::Handler::Thin.run(staticme, :Port => port, :Host => host)
+    staticme_app = Rack::Builder.new do
+
+      index = params[:index]
+      path = params[:path]
+
+      if !index.nil? && File.exists?( File.join( path, index ) )
+        map '/' do
+          run Rack::File.new( File.join( path, index ) )
+        end
+      end
+      run Rack::Directory.new( path )
+    end
+
+    Rack::Handler::Thin.run(
+      staticme_app,
+      {
+        :Port => params[:port],
+        :Host => params[:host]
+      }
+    ) do |server|
+      self.server = server
+      blk.call(server) if block_given?
+    end
+
   end
+
+  def stop!
+    self.server.stop!
+  end
+
 end
--- test/test_staticme.rb
+++ test/test_staticme.rb
@@ -1,7 +1,47 @@
 require 'helper'
+require 'net/http'
 
 class TestStaticme < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def setup
+    @params = {
+      '--path' => File.join(
+        Dir.pwd,
+        'test',
+        'assets'
+      ),
+      '--index' => 'index.html',
+      '--port' => 8080,
+      '--host' => '0.0.0.0'
+    }
   end
+
+  should "Serve index.html for the root path" do
+
+    uri = URI::HTTP.build(
+      :host => @params['--host'],
+      :port => @params['--port'],
+      :path => '/'
+    )
+
+    Staticme.run! @params do |srv|
+      Thread.new do
+        Thread.abort_on_exception = true
+        sleep 1
+        req = Net::HTTP::Get.new(uri.path)
+        res = Net::HTTP.start(uri.host, uri.port) { |http|
+          http.request(req)
+        }
+        assert_equal( 200, res.code.to_i )
+        assert_equal( res.body, IO.read(
+          File.join(
+            @params['--path'],
+            @params['--index']
+          )
+        ) )
+        srv.stop
+      end
+    end
+  end
+
 end
