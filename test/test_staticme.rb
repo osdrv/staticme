1fcc01756f313bcc180d5c715c7ac70014f0276e
4pcbr <me@4pcbr.com>
2014-02-22 00:04:20 +0100

    Added test for custom index file name. refs #1

--- test/test_staticme.rb
+++ test/test_staticme.rb
@@ -74,6 +74,41 @@ class TestStaticme < Test::Unit::TestCase
     end
   end
 
+  should 'Serve index file other than index.html if provided' do
+
+    file_name = 'index2.html'
+
+    local_params = @params
+    local_params['--index'] = file_name
+
+
+    uri = URI::HTTP.build(
+      :host => local_params['--host'],
+      :port => local_params['--port'],
+      :path => "/",
+      :index => file_name
+    )
+
+    Staticme.run! local_params do |srv|
+      Thread.new do
+        Thread.abort_on_exception = true
+        sleep 1
+        req = Net::HTTP::Get.new(uri.path)
+        res = Net::HTTP.start(uri.host, uri.port) { |http|
+          http.request(req)
+        }
+        assert_equal( 200, res.code.to_i )
+        assert_equal( res.body, IO.read(
+          File.join(
+            local_params['--path'],
+            file_name
+          )
+        ) )
+        srv.stop
+      end
+    end
+  end
+
   should 'Not serve absent file' do
 
     file_name = 'index3.html'
