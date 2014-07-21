6d5243789cb8cc10d8d7cbbc1bdfe4ca5942b3a4
4pcbr <me@4pcbr.com>
2014-07-21 23:05:04 +0200

    The fix for the double page reload issue

--- lib/staticme/events/emitters/file_changed.rb
+++ lib/staticme/events/emitters/file_changed.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'rb-fsevent'
+require 'digest/md5'
 
 module Staticme
 
@@ -10,9 +11,11 @@ module Staticme
       class FileChanged
 
         attr_accessor :fsevent
+        attr_accessor :_broadcast_tail
 
         def initialize
           self.fsevent = FSEvent.new
+          self._broadcast_tail = Hash.new
         end
 
         def bind!
@@ -26,18 +29,42 @@ module Staticme
           end
 
           fsevent.watch params[:path] do |dirs|
+            signature = get_signature(dirs)
+            next if already_broadcasted?(signature)
+            tail_broadcast(signature)
             Staticme.logger.info 'FS change, publishing the event.'
             Staticme.emit(:fs_changed, dirs)
           end
+
+          Thread.abort_on_exception = true
 
           Thread.new do
             fsevent.run
           end
 
-        end
+        end 
 
         def stop!
           fsevent.stop
+        end
+
+        protected
+
+        def get_signature(params)
+          Digest::MD5.hexdigest(params.to_s)
+        end
+
+        def already_broadcasted?(signature)
+          !self._broadcast_tail[signature].nil?
+        end
+
+        def tail_broadcast(signature)
+          self._broadcast_tail[signature] = true
+          # fsevent triggers 2 events in a row
+          # just a simple filtering
+          EM.add_timer 1 do
+            self._broadcast_tail.delete(signature)
+          end
         end
 
       end
